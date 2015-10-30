75a66a06f6a937d5308e5a4b2ee317134dd71493
Carlos <carloskikea@gmail.com>
2015-10-30 17:15:26 -0500

    Formulario de registro moteles

---
+++ db/migrate/20151030210802_add_name_to_users.rb
@@ -0,0 +1,13 @@
+class AddNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :description, :string
+    add_column :users, :URL_WebPage, :string
+    add_column :users, :URL_Video, :string
+    add_column :users, :logo, :string
+    add_column :users, :form_page, :string
+    add_column :users, :addres, :string
+    add_reference :users, :type, index: true, foreign_key: true
+    add_reference :users, :city, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030052555) do
+ActiveRecord::Schema.define(version: 20151030210802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,10 +51,23 @@ ActiveRecord::Schema.define(version: 20151030052555) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "description"
+    t.string   "URL_WebPage"
+    t.string   "URL_Video"
+    t.string   "logo"
+    t.string   "form_page"
+    t.string   "addres"
+    t.integer  "type_id"
+    t.integer  "city_id"
   end
 
+  add_index "users", ["city_id"], name: "index_users_on_city_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["type_id"], name: "index_users_on_type_id", using: :btree
 
   add_foreign_key "cities", "countries"
+  add_foreign_key "users", "cities"
+  add_foreign_key "users", "types"
 end
