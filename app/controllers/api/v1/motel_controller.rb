4798a046c9f8265e99a7b716e857408d1cd083d3
Carlos <carloskikea@gmail.com>
2015-12-04 08:53:14 -0500

    Se agrega el api por id

--- app/controllers/api/v1/motel_controller.rb
+++ app/controllers/api/v1/motel_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::MotelController < ApplicationController
 	def index
         respond_with User.all
     end
+
+    def show
+    	@motel = User.find(params[:id])
+    	if @motel.nil?
+    		render json: null
+    	else 
+    		render json: @motel
+    	end
+    end
+
+private
+
+    def motel_params
+      params.require(:motel).permit(:id, :email, :name, :description, :URL_WebPage, :URL_Video, :logo, :addres)
+    end
 end
