b059aa0b8cf3718451cacf47a8fcf9748c845744
Carlos <carloskikea@gmail.com>
2016-01-22 01:51:19 -0500

    desc

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :motel
+      get '/motelindex', to: 'motel#index_best', :as => 'motel_index_best'
     end
   end
 
