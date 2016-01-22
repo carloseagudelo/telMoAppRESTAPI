1f7a5735ceec779017e0d140666bf8ca75cfcc09
Carlos <carloskikea@gmail.com>
2016-01-21 23:10:18 -0500

    cambios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121042934) do
+ActiveRecord::Schema.define(version: 20160122030850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160121042934) do
     t.integer  "city_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "calification",           default: 0
   end
 
   add_index "users", ["city_id"], name: "index_users_on_city_id", using: :btree
